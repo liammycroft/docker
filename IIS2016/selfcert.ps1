968aa06f4cac4dd5b9805b25f31d328c7c79752e
Liam Mycroft <Liam.Mycroft@fenergo.com>
2016-11-08 11:27:34 +0000

    Self Cert PS1

---
+++ IIS2016/selfcert.ps1
@@ -0,0 +1,9 @@
+﻿Import-Module IISAdministration;
+
+$cert = New-SelfSignedCertificate -DnsName localhost -CertStoreLocation cert:\LocalMachine\My -FriendlyName localcert
+$certHash = $cert.GetCertHash()
+$sm = Get-IISServerManager;
+
+$sm.Sites["Default Web Site"].Bindings.Add("*:443:", $certHash, "My", "0");
+
+$sm.CommitChanges();
